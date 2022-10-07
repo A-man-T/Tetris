90b5b66f27dcb8e53c8f0f0e154ad84e5e865ef6
amantewari <amanatewari@gmail.com>
2022-10-07 11:55:11 -0500

    new jbrain

---
+++ src/main/java/assignment/JBrainTetris.java
@@ -0,0 +1,22 @@
+package assignment;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class JBrainTetris extends JTetris{
+    Brain dumb;
+    public static void main(String[] args) {
+        createGUI(new JBrainTetris());
+    }
+    JBrainTetris(){
+        dumb = new GoodBrain();
+
+
+        timer = new javax.swing.Timer(DELAY, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tick(dumb.nextMove(board));
+            }
+        });
+    }
+}
