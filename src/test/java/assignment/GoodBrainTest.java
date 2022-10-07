bc2a8eadfeebfcd65629037d1637a592f7574226
amantewari <amanatewari@gmail.com>
2022-10-07 12:31:24 -0500

    brain test

--- src/test/java/assignment/GoodBrainTest.java
+++ src/test/java/assignment/GoodBrainTest.java
@@ -2,10 +2,14 @@ package assignment;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoodBrainTest {
-    Board testBoard = new TetrisBoard();
+    Point origin = new Point();
+    Piece.PieceType[][] grid = new Piece.PieceType[][]{{Piece.PieceType.STICK, Piece.PieceType.STICK}};
+    //Board testBoard = new TetrisBoard(grid,0,Piece.PieceType.STICK,origin,null,null,1,1,1);
 
     @Test
     public void nextMoveTest(){
